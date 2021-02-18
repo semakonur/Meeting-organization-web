4aafecbba7b1b4036fa30964ad40db535ffb9d64
semakonur <75995747+semakonur@users.noreply.github.com>
2021-02-18 23:48:31 +0300

    toplantı düzenleme sitesi

---
+++ index.html/index.js
@@ -0,0 +1,106 @@
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let selectYear = document.getElementById("year");
+let selectMonth = document.getElementById("month");
+
+
+
+let months = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"];
+
+let monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+
+function next() {
+    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+    currentYear = parseInt(selectYear.value);
+    currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
+}
+
+
+function showCalendar(month, year) {
+
+    let firstDay = (new Date(year, month)).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
+    let tbl = document.getElementById("calendar-body"); // body of the calendar
+
+    // clearing all previous cells
+    tbl.innerHTML = "";
+
+    // filing data about month and in the page via DOM.
+    monthAndYear.innerHTML = months[month] + " " + year;
+    selectYear.value = year;
+    selectMonth.value = month;
+
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        // creates a table row
+        let row = document.createElement("tr");
+
+        //creating individual cells, filing them up with data.
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth) {
+                break;
+            }
+
+            else {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode(date);
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
+                } // color today's date
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+                      
+
+        }
+        
+        tbl.appendChild(row); // appending each row into calendar body.
+    }
+    
+    
+}
+document.getElementById("demo").innerHTML="";
+var yazı="";
+function olustur(){
+var Withh=document.getElementById("myTextwith").value;
+var Datee=document.getElementById("myTextdate").value;
+var Firstt=document.getElementById("myTextfirst").value;
+var Wheree=document.getElementById("myTextwhere").value;
+
+if(document.getElementById("demo").innerHTML==""){
+    yazı+="<tr>"+"<td>"+Firstt+"</td>"+"<td>"+Withh+"</td>"+"<td>"+Wheree+"</td>"+"<td>"+Datee+"</td>"+"</tr>";
+    
+}
+else{
+    yazı+="<tr>"+"<td>"+Firstt+"</td>"+"<td>"+Withh+"</td>"+"<td>"+Wheree+"</td>"+"<td>"+Datee+"</td>"+"</tr>";
+    
+}
+document.getElementById("demo").innerHTML=yazı;
+
+}
+
